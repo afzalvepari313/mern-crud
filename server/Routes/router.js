82c92f7a2e9e97b953c9b7cf63478880e200be04
afzal <afza313dz@gmail.com>
2023-12-17 07:55:54 +0530

    chanages

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const controllers = require("../Controllers/usersControllers");
 const upload = require("../multerconfig/storageConfig")
-const middlewares = require("../middlewares/cryptoAES")
+const middlewaresTest = require("../multerconfig/middleware")
 
 
 // routes
@@ -10,7 +10,7 @@ router.post("/user/register",upload.single("user_profile"),controllers.userpost)
 router.get("/user/details",controllers.userget);
 router.post("/user/:id",controllers.singleuserget);
 router.put("/user/edit/:id",upload.single("user_profile"),controllers.useredit);
-router.delete("/user/delete/:id",controllers.userdelete);
+router.delete("/user/delete/:id",controllers.userdelete);       
 router.put("/user/status/:id",controllers.userstatus);
 router.get("/userexport",controllers.userExport);
 
